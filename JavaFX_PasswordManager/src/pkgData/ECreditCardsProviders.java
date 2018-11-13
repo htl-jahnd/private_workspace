6509b3514c68aa637529ae5e22ecbee0d15b644c
david.jahn <david.jahn@192.168.191.3>
2018-11-13 13:07:20 +0100

    redesigned everything

--- JavaFX_PasswordManager/src/pkgData/ECreditCardsProviders.java
+++ JavaFX_PasswordManager/src/pkgData/ECreditCardsProviders.java
@@ -2,35 +2,37 @@ package pkgData;
 
 public enum ECreditCardsProviders
 {
-    Visa, MasterCard, AmericanExpress, DinersClub;
-    
-    
-    
-    public static ECreditCardsProviders getProvider(String s) {
-	switch(s) {
-	case "Visa":
-	    return Visa;
-	case "MasterCard":
-	    return MasterCard;
-	case "AmericanExpress":
-	    return AmericanExpress;
-	case "DinersClub":
-	    return DinersClub;
+	Visa, MasterCard, AmericanExpress, DinersClub;
+
+	public static ECreditCardsProviders getProvider(String s)
+	{
+		switch (s)
+		{
+			case "Visa":
+				return Visa;
+			case "MasterCard":
+				return MasterCard;
+			case "AmericanExpress":
+				return AmericanExpress;
+			case "DinersClub":
+				return DinersClub;
+		}
+		return null;
 	}
-	return null;
-    }
-    
-    public static String getProviderString(ECreditCardsProviders e) {
-	switch(e) {
-	case Visa:
-	    return "Visa";
-	case MasterCard:
-	    return "MasterCard";
-	case AmericanExpress:
-	    return "AmericanExpress";
-	case DinersClub:
-	    return "DinersClub";
+
+	public static String getProviderString(ECreditCardsProviders e)
+	{
+		switch (e)
+		{
+			case Visa:
+				return "Visa";
+			case MasterCard:
+				return "MasterCard";
+			case AmericanExpress:
+				return "AmericanExpress";
+			case DinersClub:
+				return "DinersClub";
+		}
+		return null;
 	}
-	return null;
-    }
 }
--- JavaFX_PasswordManager/src/pkgData/WebAccount.java
+++ JavaFX_PasswordManager/src/pkgData/WebAccount.java
@@ -5,78 +5,78 @@ import java.net.URL;
 public class WebAccount
 {
 
-    private String websiteName;
-    private URL websiteURL;
-    private String username;
-    private String password;
-    private String additionalInformation;
+	private String websiteName;
+	private URL websiteURL;
+	private String username;
+	private String password;
+	private String additionalInformation;
 
-    public WebAccount(String websiteName, URL websiteURL, String username, String password,
-	    String additionalInformation)
-    {
-	super();
-	this.websiteName = websiteName;
-	this.websiteURL = websiteURL;
-	this.username = username;
-	this.password = password;
-	this.additionalInformation = additionalInformation;
-    }
+	public WebAccount(String websiteName, URL websiteURL, String username, String password,
+			String additionalInformation)
+	{
+		super();
+		this.websiteName = websiteName;
+		this.websiteURL = websiteURL;
+		this.username = username;
+		this.password = password;
+		this.additionalInformation = additionalInformation;
+	}
 
-    @Override
-    public String toString()
-    {
-	return "WebAccount [websiteName=" + websiteName + ", websiteURL=" + websiteURL + ", username=" + username
-		+ ", password=" + password + ", additionalInformation=" + additionalInformation + "]";
-    }
+	@Override
+	public String toString()
+	{
+		return "WebAccount [websiteName=" + websiteName + ", websiteURL=" + websiteURL + ", username=" + username
+				+ ", password=" + password + ", additionalInformation=" + additionalInformation + "]";
+	}
 
-    public String getWebsiteName()
-    {
-	return websiteName;
-    }
+	public String getWebsiteName()
+	{
+		return websiteName;
+	}
 
-    public void setWebsiteName(String accountName)
-    {
-	this.websiteName = accountName;
-    }
+	public void setWebsiteName(String accountName)
+	{
+		this.websiteName = accountName;
+	}
 
-    public String getUsername()
-    {
-	return username;
-    }
+	public String getUsername()
+	{
+		return username;
+	}
 
-    public void setUsername(String username)
-    {
-	this.username = username;
-    }
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
 
-    public String getPassword()
-    {
-	return password;
-    }
+	public String getPassword()
+	{
+		return password;
+	}
 
-    public void setPassword(String password)
-    {
-	this.password = password;
-    }
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
 
-    public String getAdditionalInformation()
-    {
-	return additionalInformation;
-    }
+	public String getAdditionalInformation()
+	{
+		return additionalInformation;
+	}
 
-    public void setAdditionalInformation(String additionalInformation)
-    {
-	this.additionalInformation = additionalInformation;
-    }
+	public void setAdditionalInformation(String additionalInformation)
+	{
+		this.additionalInformation = additionalInformation;
+	}
 
-    public URL getWebsiteURL()
-    {
-	return websiteURL;
-    }
+	public URL getWebsiteURL()
+	{
+		return websiteURL;
+	}
 
-    public void setWebsiteURL(URL websiteURL)
-    {
-	this.websiteURL = websiteURL;
-    }
+	public void setWebsiteURL(URL websiteURL)
+	{
+		this.websiteURL = websiteURL;
+	}
 
 }
